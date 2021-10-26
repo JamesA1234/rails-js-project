98fcde6d7b867013e3b01bafb45c4fc9e33804f8
Tao Liu <taoliudesign@gmail.com>
2021-10-25 21:45:44 -0700

    add serializers

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -1,18 +1,23 @@
 class WorkoutsController < ApplicationController
     def index
-
+        @workouts = Workout.all
+        render json: @workouts
     end
     
     def show
-
+        @workout = Workout.find(params[:id])
+        render json: @workout
     end
-
+    
     def create
-
+        @workout = Workout.find_or_create_by(params[:id])
+        render json: @workout
     end
-
+    
     def destroy
-
+        @workout = Workout.find(params[:id])
+        @workout.destroy
+        render json: @workout
     end
     
 end
